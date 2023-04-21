cdff0f0b0cb32149fe9d85da770c0ff4eae9b406
Omega <79137917+OmegaLeWolf@users.noreply.github.com>
2023-04-21 15:08:05 +0000

    Added Battery state and Perc to slstatus

--- config.h
+++ config.h
@@ -67,6 +67,8 @@ static const struct arg args[] = {
 	//{ wifi_perc, "W: (%3s%% on ", "wlp8s0" },
     //{ netspeed_rx, "%sB/s  ", "enp0s3" },
 	{ run_command, "[VOL: %4s]   ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
+	{ battery_perc, "[BAT  %s%% ", "BAT0"         },
+	{ battery_state, "%s]   ", "BAT0"             },
 	{ cpu_perc, "[CPU  %s%%]   ", NULL	      },
 	{ ram_perc, "[RAM  %s%%]   ", NULL	      },
 	{ datetime, "%s",           "%a %b %d %r" },
